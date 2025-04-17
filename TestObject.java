3fa3e31f721d12ab27bc0b7678b1cac15d3875dd
Serena Hawkins <152425961+serenaHawkins314@users.noreply.github.com>
2025-04-17 16:01:17 -0700

    TestObject.java (FINAL VERSION)

--- TestObject.java
+++ TestObject.java
@@ -8,6 +8,7 @@ public class TestObject extends DrawableObject
    public TestObject(float x, float y)
    {
       super(x,y);
+      System.out.println("hello");
    }
    //draws itself at the passed in x and y.
    public void drawMe(float x, float y, GraphicsContext gc)
@@ -16,5 +17,7 @@ public class TestObject extends DrawableObject
       gc.fillOval(x-14,y-14,27,27);
       gc.setFill(Color.GRAY);
       gc.fillOval(x-13,y-13,25,25);
+      gc.setFill(Color.CYAN);
+      gc.fillOval(x-10,y-10,19,19);
    }
 }
